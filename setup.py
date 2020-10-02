fbaadd464b6cfcc7ab3dd7ed925ccbc5cb9ec66b
Jason LeBrun <jblebrun@gmail.com>
2020-10-01 21:39:33 -0700

    Add a setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='OctoPy',
+    version='0.9dev',
+    packages=['octopy',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
